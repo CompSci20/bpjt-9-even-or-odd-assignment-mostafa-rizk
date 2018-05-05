38e7bd44db7780e4a049691fd5d9311520660256
Mostafa Rizk <m.rizk.student@gmail.com>
2018-05-05 04:39:05 -0600

    Declared variables and asked user for input

--- Tester.java
+++ Tester.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 public class Tester {
    public static void main(String args[]) {
-      
+      Scanner kbReader = new Scanner(System.in);
+      System.out.print("Enter an integer: ");
+      int userInt = kbReader.nextInt();
+      string oddEven;
    }
 }
